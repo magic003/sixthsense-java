7b57c2b3cdffa53775fe989d5dc10cf8f66dd285
Minjie Zha <minjiezha@gmail.com>
2012-12-13 22:54:56 +0800

    set window size and background

--- src/wuw/WUW.java
+++ src/wuw/WUW.java
@@ -5,6 +5,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.scene.paint.Color;
 
 public class WUW extends Application {
     public static void main(String[] args) {
@@ -15,7 +17,10 @@ public class WUW extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("wuw.fxml"));
 
         stage.setTitle("WUW");
-        stage.setScene(new Scene(root, 300, 275));
+        stage.initStyle(StageStyle.UNDECORATED);
+        Scene scene = new Scene(root, 1024, 768);
+        scene.setFill(Color.BLACK);
+        stage.setScene(scene);
         stage.show();
     }
 }
